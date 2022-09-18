f9d985bdbe8fbbb371518aba6b596d0b343c3184
Rai Naveed <rainaveed654@gmail.com>
2022-09-19 01:29:06 +0500

    add

--- lazy loading in front-end/front-end/src/app/adminmodule/adminmodule-routing.module.ts
+++ lazy loading in front-end/front-end/src/app/adminmodule/adminmodule-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './admin-component/dashboard/dashboard.component';
+import { ProductsComponent } from './admin-component/products/products.component';
 import { AdminmoduleComponent } from './adminmodule.component';
 
 const routes: Routes = [{ path: '', component: AdminmoduleComponent,
 children:[
-  {path:'',component:DashboardComponent}
+  {path:'',component:DashboardComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'products', component:ProductsComponent}
 ]
 }];
 
--- lazy loading in front-end/front-end/src/app/app-routing.module.ts
+++ lazy loading in front-end/front-end/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) },
    { path: 'MainPortal', loadChildren: () => import('./main-module/main-module.module').then(m => m.MainModuleModule) },
-    { path: 'adminmodule', loadChildren: () => import('./adminmodule/adminmodule.module').then(m => m.AdminmoduleModule) },
+    { path: 'admin', loadChildren: () => import('./adminmodule/adminmodule.module').then(m => m.AdminmoduleModule) },
   { path: 'managementmodule', loadChildren: () => import('./managementmodule/managementmodule.module').then(m => m.ManagementmoduleModule) }
   ];
 
